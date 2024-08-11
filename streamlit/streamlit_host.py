6f0126f6effb8d57aa9c6b33506559d462e199d3
Demiga-g <midegageorge2@gmail.com>
2024-08-11 16:39:07 +0300

    checked if file meets pep8 standards

--- streamlit/streamlit_host.py
+++ streamlit/streamlit_host.py
@@ -1,7 +1,10 @@
-import streamlit as st
-import pickle
-import numpy as np
+# pylint: disable=missing-module-docstring
 import os
+import pickle
+
+import numpy as np
+
+import streamlit as st
 
 # # Set up the page configuration
 st.set_page_config(page_title="Crop Recommendation", layout="centered")
@@ -16,12 +19,18 @@ st.title("Crop Recommendation")
 
 col1, col2 = st.columns(2)
 
-with st.form(key='crop_form'):
+with st.form(key="crop_form"):
     with col1:
         N = st.number_input("N (Nitrogen content in soil):", min_value=0, max_value=250)
-        P = st.number_input("P (Phosphorus content in soil):", min_value=0, max_value=250)
-        K = st.number_input("K (Potassium content in soil):", min_value=0, max_value=250)
-        temperature = st.number_input("Temperature (in Celsius):", min_value=5, max_value=50)
+        P = st.number_input(
+            "P (Phosphorus content in soil):", min_value=0, max_value=250
+        )
+        K = st.number_input(
+            "K (Potassium content in soil):", min_value=0, max_value=250
+        )
+        temperature = st.number_input(
+            "Temperature (in Celsius):", min_value=5, max_value=50
+        )
 
     with col2:
         humidity = st.number_input("Humidity (in %):", min_value=5, max_value=100)
@@ -43,10 +52,15 @@ if submit_button:
             prediction = model.predict(data)
             output = prediction[0]
 
-            st.markdown(f"<h2>Recommended crop is {output}</h2>", unsafe_allow_html=True)
+            st.markdown(
+                f"<h2>Recommended crop is {output}</h2>", unsafe_allow_html=True
+            )
         except ValueError:
-            st.markdown("<h2>Please ensure all inputs are valid numbers.</h2>", unsafe_allow_html=True)
+            st.markdown(
+                "<h2>Please ensure all inputs are valid numbers.</h2>",
+                unsafe_allow_html=True,
+            )
     else:
         st.markdown("<h2>Please fill in all fields.</h2>", unsafe_allow_html=True)
 
-st.markdown('</div>', unsafe_allow_html=True)
+st.markdown("</div>", unsafe_allow_html=True)
