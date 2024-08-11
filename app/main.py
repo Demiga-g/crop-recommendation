688bc85e47d92572c0e13ef053d8cb69e78bd76a
Demiga-g <midegageorge2@gmail.com>
2024-08-11 13:22:14 +0300

    configured code for a flask app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,32 @@
 import pickle
+import numpy as np
+from flask import Flask, request, render_template
 
 
+app = Flask(__name__)
 
-with open("NBClassifier.pkl", "rb") as f_in:
-  model = pickle.load(f_in)
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+
+@app.route('/prediction', methods=['POST'])
+def predict_endpoint():
+  """This function serves as the API endpoint for the recommendation prediction."""
   
-def predict_recommendation(features):
-  prediction = model.predict(features)
-  return prediction
+  # Load model
+  with open("../model/NBClassifier.pkl", "rb") as f_in:
+    model = pickle.load(f_in)
+  
+  # get data
+  form_values = [float(x) for x in request.form.values()]
+  data = [np.array(form_values)]
+  
+  # get prediction
+  prediction = model.predict(data)
+  output = prediction[0]
+  
+  return render_template('index.html', prediction_text=f"Recommended crop is {output}")
+
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port=9696)
