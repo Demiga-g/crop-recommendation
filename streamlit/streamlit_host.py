9ec098c7031812ff8ba54b3b73bb30d1519b8d9d
Demiga-g <midegageorge2@gmail.com>
2024-08-11 17:05:24 +0300

    check if file meet pep8 standards

--- streamlit/streamlit_host.py
+++ streamlit/streamlit_host.py
@@ -7,37 +7,46 @@ import numpy as np
 import streamlit as st
 
 # # Set up the page configuration
-st.set_page_config(page_title="Crop Recommendation", layout="centered")
+st.set_page_config(page_title='Crop Recommendation', layout='centered')
 
 # Load the model
-model_path = os.path.join(os.path.dirname(__file__), "..", "model", "NBClassifier.pkl")
-with open(model_path, "rb") as f_in:
+model_path = os.path.join(
+    os.path.dirname(__file__), '..', 'model', 'NBClassifier.pkl'
+)
+with open(model_path, 'rb') as f_in:
     model = pickle.load(f_in)
 
 
-st.title("Crop Recommendation")
+st.title('Crop Recommendation')
 
 col1, col2 = st.columns(2)
 
-with st.form(key="crop_form"):
+with st.form(key='crop_form'):
     with col1:
-        N = st.number_input("N (Nitrogen content in soil):", min_value=0, max_value=250)
-        P = st.number_input(
-            "P (Phosphorus content in soil):", min_value=0, max_value=250
+        N = st.number_input(
+            'N (Nitrogen content in soil):', min_value=0, max_value=250
         )
+        P = st.number_input(
+            'P (Phosphorus content in soil):', min_value=0, max_value=250
+        )
+
         K = st.number_input(
-            "K (Potassium content in soil):", min_value=0, max_value=250
+            'K (Potassium content in soil):', min_value=0, max_value=250
         )
         temperature = st.number_input(
-            "Temperature (in Celsius):", min_value=5, max_value=50
+            'Temperature (in Celsius):', min_value=5, max_value=50
         )
 
     with col2:
-        humidity = st.number_input("Humidity (in %):", min_value=5, max_value=100)
-        ph = st.number_input("pH level of soil:", min_value=1, max_value=14)
-        rainfall = st.number_input("Rainfall (in mm):", min_value=0, max_value=300)
+        humidity = st.number_input(
+            'Humidity (in %):', min_value=5, max_value=100
+        )
+        ph = st.number_input('pH level of soil:', min_value=1, max_value=14)
+        rainfall = st.number_input(
+            'Rainfall (in mm):', min_value=0, max_value=300
+        )
 
-    submit_button = st.form_submit_button(label="Predict")
+    submit_button = st.form_submit_button(label='Predict')
 
 
 # Handle form submission
@@ -53,14 +62,16 @@ if submit_button:
             output = prediction[0]
 
             st.markdown(
-                f"<h2>Recommended crop is {output}</h2>", unsafe_allow_html=True
+                f'<h2>Recommended crop is {output}</h2>', unsafe_allow_html=True
             )
         except ValueError:
             st.markdown(
-                "<h2>Please ensure all inputs are valid numbers.</h2>",
+                '<h2>Please ensure all inputs are valid numbers.</h2>',
                 unsafe_allow_html=True,
             )
     else:
-        st.markdown("<h2>Please fill in all fields.</h2>", unsafe_allow_html=True)
+        st.markdown(
+            '<h2>Please fill in all fields.</h2>', unsafe_allow_html=True
+        )
 
-st.markdown("</div>", unsafe_allow_html=True)
+st.markdown('</div>', unsafe_allow_html=True)
