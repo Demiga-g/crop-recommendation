ea99be9cb9f7ba4a336aca945765fc0cc1d4519a
Demiga-g <midegageorge2@gmail.com>
2024-08-14 15:42:31 +0300

    dropped the use of  `st_pages` library

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,12 +1,17 @@
 # pylint: disable=no-member
-from st_pages import get_nav_from_toml
+# from st_pages import get_nav_from_toml
 
 import streamlit as st
 
 st.set_page_config(layout="wide")
 
-nav = get_nav_from_toml(".streamlit/pages_sections.toml")
+# nav = get_nav_from_toml(".streamlit/pages_sections.toml")
 
-pg = st.navigation(nav)
+pg = st.navigation(
+    [
+        st.Page("home.py", title="About", icon="🌿"),
+        st.Page("app_content.py", title="Toolkit", icon="💻"),
+    ]
+)
 
 pg.run()
