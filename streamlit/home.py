ff83d264e1d0128521320da59ae131d75b7babbd
Demiga-g <midegageorge2@gmail.com>
2024-08-14 14:33:19 +0300

    added info to guide users about the app

---
+++ streamlit/home.py
@@ -0,0 +1,29 @@
+# pylint: disable=no-member
+import streamlit as st
+
+st.title("The Crop Recommendation App")
+
+st.markdown(
+    """
+### About the App!
+
+This application is designed to assist farmers and agricultural professionals in making
+informed decisions about which crops to plant based on specific environmental conditions.
+
+### Features:
+
+- Enter soil nutrient levels (Nitrogen, Phosphorus, Potassium) and environmental factors
+(Temperature, Humidity, pH, Rainfall).
+- Get a recommendation for the best crop to plant.
+- View detailed growing conditions for the recommended crop.
+
+*Use the sidebar to navigate to the "Toolkit" and start using the app.*
+
+### Disclaimer
+
+*The recommendations provided by this app are based on data and models that analyze specific
+environmental factors such as soil content, temperature, humidity, and rainfall. While we strive
+to offer accurate and reliable advice, the outputs are intended for informational purposes only
+and should not be considered as professional agricultural guidance.*
+"""
+)
