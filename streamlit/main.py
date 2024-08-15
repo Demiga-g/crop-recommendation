637a4a504666c154307698e1e3f22225e1157b8a
Demiga-g <midegageorge2@gmail.com>
2024-08-15 10:47:38 +0300

    added copyrights to navigation bar

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,5 +1,6 @@
 # pylint: disable=no-member
 # from st_pages import get_nav_from_toml
+from datetime import datetime
 
 import streamlit as st
 
@@ -12,6 +13,19 @@ pg = st.navigation(
         st.Page("home.py", title="About", icon="🌿"),
         st.Page("app_content.py", title="Toolkit", icon="💻"),
     ]
+)
+
+# copyright section
+current_year = datetime.now().year
+st.sidebar.markdown(
+    f"""
+                    <br><br><br><br><br><br>
+                    © {current_year}
+                    <br>
+                    Midega George
+                    <br>
+                    Unlimited rights 😎.""",
+    unsafe_allow_html=True,
 )
 
 pg.run()
